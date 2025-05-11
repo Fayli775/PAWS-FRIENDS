0abd4f27ca238cae667b9789e4504bd8f52b99ff
junlong <wjlinnz@gmail.com>
2025-05-11 16:20:46 +1200

    disable lint rules

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslintConfig: {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  }
 };
 
 export default nextConfig;
