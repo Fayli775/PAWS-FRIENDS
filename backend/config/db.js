8c0448bfdea7d5a781a5accb357bd2333844b97a
junlong <wjlinnz@gmail.com>
2025-05-11 23:22:02 +1200

    upgrade db connection limit

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 5,
+  connectionLimit: process.env.DB_CONNECTION_LIMIT ? parseInt(process.env.DB_CONNECTION_LIMIT, 10) : 10,
   queueLimit: 0,
   idleTimeout: 60000,
   enableKeepAlive: true,
